7fee956f6862455595fbe94d6dccf531a8d95b9e
keshav-arora197 <keshavarora778@gmail.com>
2026-04-05 23:19:19 +0530

    Add files via upload

---
+++ Day_64.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int queue[MAX], front = -1, rear = -1;
+int visited[MAX];
+
+// Queue operations
+void enqueue(int x) {
+    if (rear == MAX - 1) return;
+    if (front == -1) front = 0;
+    queue[++rear] = x;
+}
+
+int dequeue() {
+    if (front == -1 || front > rear) return -1;
+    return queue[front++];
+}
+
+// BFS function
+void bfs(int adj[MAX][MAX], int n, int start) {
+    enqueue(start);
+    visited[start] = 1;
+
+    while (front <= rear) {
+        int node = dequeue();
+        printf("%d ", node);
+
+        for (int i = 0; i < n; i++) {
+            if (adj[node][i] == 1 && visited[i] == 0) {
+                enqueue(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, start;
+    int adj[MAX][MAX];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    // Initialize visited array
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    printf("BFS Traversal: ");
+    bfs(adj, n, start);
+
+    return 0;
+}
